784e62d2545aa79b04d39e3bb443cc2891267952
zelleg <massyl.zelleg2201@gmail.com>
2022-10-03 14:26:18 +0200

    last version

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -1,11 +1,11 @@
 using Leonardo;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/Fibonacci", 
-     () => Fibonacci.RunAsync(new []{"44", "43"}));
-
+app.MapGet("/Fibonacci", async () =>
+{
+    await using var dataContext = new FibonacciDataContext();
+    return new Fibonacci(dataContext).RunAsync(new[] { "44", "43" });
+});
 app.Run();
