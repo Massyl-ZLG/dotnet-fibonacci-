ad193770b94a7de516567bd77d72deafb02c5ded
zelleg <massyl.zelleg2201@gmail.com>
2022-10-10 13:40:46 +0200

    add last version

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,63 +1,40 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using Leonardo;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+string environmentName = null;
+IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+    .AddEnvironmentVariables().AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{environmentName}.json", true, true).Build();
+
+var applicationSection = configuration.GetSection("Application");
+var applicationConfig = applicationSection.Get<ApplicationConfig>();
 
 var services = new ServiceCollection();
 services.AddTransient<FibonacciDataContext>();
 services.AddTransient<Fibonacci>();
 services.AddLogging(configure => configure.AddConsole());
 
-using(var serviceProvider = services.BuildServiceProvider()) {
-    var logger =serviceProvider.GetService<ILogger<Program>>();
+await using var serviceProvider = services.BuildServiceProvider();
+var logger = serviceProvider.GetService<ILogger<Program>>();
                                                                                                                                     
-    logger.LogInformation($"Application Name : {applicationConfig.Name}");
-    logger.LogInformation($"Application Message : {applicationConfig.Message}");
+logger.LogInformation($"Application Name : {applicationConfig.Name}");
+logger.LogInformation($"Application Message : {applicationConfig.Message}");
 
-    var fibonacci = serviceProvider.GetService<Fibonacci>();
-    var results = await fibonacci.Execute(args);
-}
+var fibonacci = serviceProvider.GetService<Fibonacci>();
+var results = await fibonacci.RunAsync(args);
 
-
-var loggerFactory = LoggerFactory.Create(builder => {
-        builder.AddFilter("Microsoft", LogLevel.Warning)
-            .AddFilter("System", LogLevel.Warning)
-            .AddFilter("Demo", LogLevel.Debug)
-            .AddConsole();
-    }
-);
-var logger = loggerFactory.CreateLogger("Demo.Program");
 
 logger.LogInformation($"Application Name : {applicationConfig.Name}");
 logger.LogInformation($"Application Message : {applicationConfig.Message}");
 
-/*
-var stopwatch = new Stopwatch();
-
-stopwatch.Start();
-//await using var dataContext = new FibonacciDataContext();
-var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
-var dataBaseName = Guid.NewGuid().ToString();
-builder.UseInMemoryDatabase(dataBaseName);
-var options = builder.Options;
-var fibonacciDataContext = new FibonacciDataContext(options);
-await fibonacciDataContext.Database.EnsureCreatedAsync();
-
-var listOfResults = await new Fibonacci(dataContext).RunAsync(args);
-
-foreach (var listOfResult in listOfResults)
+public record ApplicationConfig
 {
-    Console.WriteLine($"Result : {listOfResult}");
+    public string Name { get; set; }         
+    public string Message { get; set; }
 }
-stopwatch.Stop();
-
-Console.WriteLine("time elapsed in seconds : " + stopwatch.Elapsed.Seconds);
-*/
-// int Fib(int i)
-// {
-//     if (i <= 2) return 1;
-//     return Fib(i - 1) + Fib(i - 2);
-// }  
-
